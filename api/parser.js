18d43e99f61ac3616a200435a1d8fae379b22f24
Richard-NG <richard.chihong.ng@gmail.com>
2018-11-27 12:07:47 +0800

    api parser

---
+++ api/parser.js
@@ -0,0 +1,34 @@
+const data = require("./app.json");
+
+let level = 0;
+
+function spacer(level) {
+  return "  ".repeat(level);
+}
+
+function parse(data, level) {
+  const skipKeys = ["widget", "arguements"];
+  const className = data["widget"];
+  let widget = `${className}(\n`;
+
+  Object.keys(data).forEach(key => {
+    if (skipKeys.includes(key)) {
+      return;
+    }
+
+    const value = data[key];
+    if (typeof value === "object" && !Array.isArray(value)) {
+      level += 1;
+      widget += `${spacer(level)}${key}: ${parse(value, level)},\n`;
+      level -= 1;
+    } else {
+      level += 1;
+      widget += `${spacer(level)}${key}: ${value},\n`;
+      level -= 1;
+    }
+  });
+
+  return widget + `${spacer(level)})`;
+}
+
+console.log(parse(data, level));
